1cd4b2c7b7e2a76acc0ecacc53163a0a20427cf7
Ahmad-RAza579 <you@example.com>
2024-10-01 17:43:01 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,6 +1,8 @@
-a=(1,2,3,4,'hello')
-print(a)
-print(a[2])
-print(a[2:4])
-print(len(a))
-print(a)
+s1={1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4}
+s2={1,3,23,4,5,5,5,4,4,2,7,8,5,6,0,9}
+print(s1)
+print(s2)
+print(s1.intersection(s2))
+print(s1.union(s2))
+print(s1-s2)
+print(s1.symmetric_difference(s2))
