309d594afc6c61a6b5ddb3083684ef849a54a30a
Ahmad-RAza579 <you@example.com>
2024-10-02 17:26:00 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,8 +1,5 @@
-s1={1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,4}
-s2={1,3,23,4,5,5,5,4,4,2,7,8,5,6,0,9}
-print(s1)
-print(s2)
-print(s1.intersection(s2))
-print(s1.union(s2))
-print(s1-s2)
-print(s1.symmetric_difference(s2))
+a=[1,2,3,4]
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
