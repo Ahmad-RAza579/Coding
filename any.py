44ad15c00789965c5fab2ef536da77fc62429c90
Ahmad-RAza579 <you@example.com>
2024-10-08 17:22:55 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,9 +1,10 @@
-def largest_number(i):
-    largest = i[0]  
-    for num in i:    
-        if num > largest:
-            largest = num   
-    return largest
-my_list = [11,54,76,87,98,79,0.9,99,67,900,1049]
-a = largest_number(my_list)
-print(a)
+class Student:
+    print('i am inside the class')
+    def __init__(self,name):
+        self.name=name
+    
+    def display(self):
+        print("my name is",self.name)
+
+a=Student("ahmed")
+a.display()
