50f920fb93aa95427def919f30dc4b416beba49d
Ahmad-RAza579 <you@example.com>
2024-10-15 17:30:16 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,7 +1,15 @@
-class library:
-    def __init__(self):
-        print("This is a library")
-ob=library()
-ob1=library()
-del ob
-print(ob1)
+class dad:
+    def __init__(self,eyes,aggressive):
+        self.eyes = eyes
+        self.aggresive = aggressive
+    def display(self):
+        print("your eye colour is", self.eyes)
+        print("you are aggresive", self.aggresive)
+
+class son(dad):
+    def __init__(self,name,age,eyes,agressive):
+        self.name = name
+        self.age = age
+        dad.__init__(self,eyes,agressive)
+obj = son("penguin", 8, "blue", True)
+obj.display()
