a62a5d1db4953783fb0c6bb05e1d540ff884d88d
Ahmad-RAza579 <you@example.com>
2024-10-15 17:44:43 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,15 +1,15 @@
-class dad:
-    def __init__(self,eyes,aggressive):
-        self.eyes = eyes
-        self.aggresive = aggressive
-    def display(self):
-        print("your eye colour is", self.eyes)
-        print("you are aggresive", self.aggresive)
-
-class son(dad):
-    def __init__(self,name,age,eyes,agressive):
-        self.name = name
-        self.age = age
-        dad.__init__(self,eyes,agressive)
-obj = son("penguin", 8, "blue", True)
-obj.display()
+from abc import ABC
+class Animal(ABC):
+    def move(self):
+        pass
+class Human(Animal):
+    def move (self):
+        print("human walk with 2 legs")
+class Snake(Animal):
+    def move(self):
+        print("snake crawl with their body")
+a=Animal()
+b=Human()
+b.move()
+c=Snake()
+c.move()
