2b22febf29fdbf6929800876a31ec5bb9ad0bdc6
Ahmad-RAza579 <you@example.com>
2024-10-22 17:27:24 +0200

    codingalproject

--- any.py
+++ any.py
@@ -1,13 +1,10 @@
-class Computer:
-  def __init__(self):
-    self.__maxprice=1000
-  def sell(self):
-    print("Selling price is",self.__maxprice)
-  def setmaxprice(self,price):
-    self.__maxprice =price
-a=Computer()
-a.sell()
-a.__maxprice=2000
-a.sell()
-a.setmaxprice(2000)
-a.sell()
+
+file=open('file.txt','r')
+print(file.read())
+file.close()
+file_write=open('file.txt','w')
+file_write.write("Hii i am Raza i love to do coding")
+file_write.close()
+file_append=open("file.txt",'a')
+file_append.write("Hello i am Heet")
+file_append.close()
